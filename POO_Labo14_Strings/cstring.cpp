5fc889c65eaa62a4fe5ed9e06cbe77b4b8a32d44
Unknown <jeremie.chatillon@heig-vd.ch>
2018-03-28 15:30:53 +0200

    ok

--- POO_Labo14_Strings/cstring.cpp
+++ POO_Labo14_Strings/cstring.cpp
@@ -154,7 +154,7 @@ String String::plus(const String& s) const throw (std::bad_alloc){
 
 String String::plus(const char* s) const throw (std::bad_alloc){
     size_t tmpLen = lenght();
-    char* tmpbuf = new char[tmpLen + strlen(s) + 1];
+    char* tmpbuf = char[tmpLen + strlen(s) + 1];
     
     strcpy(tmpbuf, str);
     strcat(tmpbuf, s);
